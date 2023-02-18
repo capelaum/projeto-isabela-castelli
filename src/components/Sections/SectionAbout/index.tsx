0d762d8967a0cf7325b1a39aa8843cc6e8fbfb7d
capelaum <thecapellett@gmail.com>
2023-02-18 18:31:40 -0300

    ✨ update content

--- src/components/Sections/SectionAbout/index.tsx
+++ src/components/Sections/SectionAbout/index.tsx
@@ -39,7 +39,7 @@ export function SectionAbout() {
           <AboutDescription>
             <Text asChild>
               <p>
-                Sou Psicóloga, possuo o CRP 01/18851 Doutoranda em Psicologia
+                Sou Psicóloga, possuo o CRP 01/18851. Doutoranda em Psicologia
                 pela Universidade de Brasília. Pós-Graduanda em Terapia
                 Cognitivo Comportamental pelo Instituto Brasileiro de
                 Neuropsicologia e Ciências Cognitivas.
