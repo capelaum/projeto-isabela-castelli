82cfe27cb123d045ec4d3f2c5ecc2e080c0b8d01
capelaum <thecapellett@gmail.com>
2023-03-10 17:35:13 -0300

    📈 update GTM scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,22 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
+          {/* <!-- Google Tag Manager --> */}
+          <Script
+            id={process.env.NEXT_PUBLIC_GTM_ID}
+            type="text/javascript"
+            async
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script',${process.env.NEXT_PUBLIC_GTM_ID})`
+            }}
+          />
+          {/* <!-- End Google Tag Manager --> */}
+
           <style
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
@@ -34,42 +50,21 @@ export default class MyDocument extends Document {
             description="A sua saúde mental é prioridade! Um acompanhamento psicológico bem feito vai ajudá-lo a compreender os seus sentimentos, seu modo de pensar e de agir."
             url={process.env.NEXT_PUBLIC_URL}
           />
-
-          {/* <!-- Google Tag Manager --> */}
-          <Script
-            id="GTM-WL3W5L5"
-            type="text/javascript"
-            async
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(w, d, s, l, i) {
-                    w[l] = w[l] || []
-                    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
-                    const f = d.getElementsByTagName(s)[0],
-                      j = d.createElement(s),
-                      dl = l != "dataLayer" ? "&l=" + l : ""
-                    j.async = true
-                    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
-                    f.parentNode.insertBefore(j, f)
-                  })(window,document,'script','dataLayer',"GTM-WL3W5L5")`
-            }}
-          />
-          {/* <!-- End Google Tag Manager --> */}
         </Head>
         <body>
-          <Main />
-          <NextScript />
-
           {/* <!-- Google Tag Manager (noscript) --> */}
           <noscript>
             <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=GTM-WL3W5L5`}
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
             ></iframe>
           </noscript>
           {/* <!-- End Google Tag Manager (noscript) --> */}
+
+          <Main />
+          <NextScript />
         </body>
       </Html>
     )
