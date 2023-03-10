386335d24d372b1a1dcf01bebaafd876e04821ed
capelaum <thecapellett@gmail.com>
2023-03-10 18:34:23 -0300

    📈 move GTM sript to _app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,6 @@
 import { Favicon } from 'components/Head/Favicon'
 import { SEO } from 'components/Head/SEO'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 import { getCssText } from 'styles/stitches.config'
 
 export default class MyDocument extends Document {
@@ -21,22 +20,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@700&display=swap"
             rel="stylesheet"
           />
-
-          {/* <!-- Google Tag Manager --> */}
-          <Script
-            id={process.env.NEXT_PUBLIC_GTM_ID}
-            type="text/javascript"
-            async
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script',${process.env.NEXT_PUBLIC_GTM_ID})`
-            }}
-          />
-          {/* <!-- End Google Tag Manager --> */}
 
           <style
             id="stitches"
