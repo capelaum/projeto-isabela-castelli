4b2b73865170c3614511ad84cd76197a536a481a
capelaum <thecapellett@gmail.com>
2023-04-20 10:44:06 -0300

    ✨ add @vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
 import { globalStyles } from 'styles/global'
@@ -7,6 +8,7 @@ globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Analytics />
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
